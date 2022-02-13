336f28606552d3e3886e7896315a983ffc5a893d
Kin1599 <dkonopliannikov05@mail.ru>
2022-02-14 02:46:51 +0400

    first commit

---
+++ grainParser.py
@@ -0,0 +1,196 @@
+import requests
+import fake_useragent
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+user = fake_useragent.UserAgent().random
+
+header = {'user-agent': user}
+
+#Cсылка на агро сервер поиск
+# linkAgroServerSearch = 'https://agroserver.ru/len/'
+linkAgroServerSearchSaflor = 'https://agroserver.ru/saflor/'
+#Cсылка на агро сервер
+linkAgroServer = 'https://agroserver.ru'
+#Ссылка на agro russia
+# linkAgroRussiaSearch = 'https://agro-russia.com/ru/trade/?adv_search=1&r_id=53&types_id=2&page='
+linkAgroRussiaSearchSaflor = 'https://agro-russia.com/ru/trade/?adv_search=1&r_id=790&types_id=2&page='
+
+
+arrName = []
+arrLink = []
+arrPrice = []
+arrCity = []
+arrDate = []
+arrOrg = []
+
+dictResult = {
+    'Название': '',
+    'Цена': '',
+    'Город': '', 
+    'Опубликовано': '',
+    'Организация': '',
+    'Ссылка': ''
+}
+
+#Функция получения кода страницы
+def getSoup(link):
+    response = requests.get(link, headers = header)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    return soup
+
+#Добавление в массивы
+def additionArr(name, city, price, date, org, link):
+    arrName.append(name)
+    arrLink.append(link)
+    arrCity.append(city)
+    arrPrice.append(price)
+    arrDate.append(date)
+    arrOrg.append(org)
+
+#Функция записи данных в excel
+def excelEntry(name, city, price, date, org, link):
+        dictResult['Название'] = name
+        dictResult['Город'] = city
+        dictResult['Цена'] = price
+        dictResult['Опубликовано'] = date
+        dictResult['Организация'] = org
+        dictResult['Ссылка'] = link
+
+        dataFrame = pd.DataFrame(dictResult)
+        dataFrame.to_excel("./Dad.xlsx", index=False) 
+
+        print('Успешно перезаписан excel-файл')
+
+#Информация с Агро Сервера
+def mainAgroServer():
+    indexPage = 1
+    
+    soupAgroServer = getSoup(linkAgroServerSearchSaflor)
+
+    #Получение кол-ва страниц
+    pages = soupAgroServer.find('ul', class_ = 'pg')
+
+    if not pages:
+        pages = ['1']
+    else:
+        pages = pages.find_all('li')
+    
+    while indexPage <= len(pages):
+        # currentLink = 'https://agroserver.ru/len/Y2l0eT18cmVnaW9uPXxjb3VudHJ5PXxtZXRrYT18c29ydD0x/' + str(indexPage) + "/"
+        currentLink = linkAgroServerSearchSaflor
+
+        soupAgroServerPage = getSoup(currentLink)
+
+        #Получение всех товаров на странице
+        itemsAgroServer = soupAgroServerPage.find_all('div', class_ = 'line')
+
+        
+        for i in range(0, len(itemsAgroServer)):
+            itemName = itemsAgroServer[i].find('div', class_ = 'th')
+            itemLink = linkAgroServer + itemName.find('a').get('href')
+            itemPrice = itemsAgroServer[i].find('div', class_ = 'price')
+            itemData = itemsAgroServer[i].find('div', class_ = 'date')
+            itemOrg = itemsAgroServer[i].find('a', class_ = 'personal_org_menu')
+            itemGeo = itemsAgroServer[i].find('div', class_ = 'geo').text.strip()
+            
+            #Если геолокация начинается с г.
+            if itemGeo.find('г.') == 0:
+                #Записываем без г.
+                newitemGeo = itemGeo[3:].strip()
+            #Если геолокация начинается с доставка
+            elif itemGeo.find('доставка') == 0:
+                newitemGeo = itemGeo.strip()
+            else:
+                newitemGeo = itemGeo.strip()
+            
+            #Если есть цена у товара
+            if itemPrice:
+                print(f"{itemName.text} за {itemPrice.text} опубликован {itemData.text.strip()} у {itemOrg.text.strip()}-> {itemLink}")
+                additionArr(itemName.text, newitemGeo, itemPrice.text, itemData.text.strip(), itemOrg.text.strip(), itemLink)
+            else:
+                print('Цена не указана')
+                additionArr(itemName.text, newitemGeo, 'Цена не указана', itemData.text.strip(), itemOrg.text.strip(), itemLink)
+
+            time.sleep(0.5)
+        
+        print(f'[INFO:] Обработал {indexPage}/{len(pages)}')
+
+        indexPage += 1
+
+def mainAgroRussia():
+    indexPage = 1
+
+    soupAgroRussia = getSoup(linkAgroRussiaSearchSaflor + str(indexPage))
+
+    pages = soupAgroRussia.find('span', class_ = 'list')
+
+    if not pages:
+        pages = ['1']
+    else:
+        pages = pages.find_all('a', class_ = 'page')
+
+    while indexPage <= len(pages) + 1:
+        currentLink = linkAgroRussiaSearchSaflor + str(indexPage)
+
+        soupAgroRussiaPage = getSoup(currentLink)
+
+        itemsAgroRussia = soupAgroRussiaPage.find_all('div', class_ = 'i_l_i_c_mode3')
+
+        for i in range(0, len(itemsAgroRussia)):
+            itemName = itemsAgroRussia[i].find('a', class_ = 'i_title')
+            if itemName:
+                itemLink = itemName.get('href')
+                itemPrice = itemsAgroRussia[i].find('span', class_ = 'i_price')
+                if itemPrice:
+                    soupAgroRussiaItem = getSoup(itemLink)
+                
+                    itemDate = soupAgroRussiaItem.find('time')
+                    itemCity = soupAgroRussiaItem.find('span', itemprop = 'addressLocality')
+                    itemOrg = soupAgroRussiaItem.find('div', class_ = 'ct_user_box_7_1')
+
+                    if itemOrg:
+                        itemOrg = itemOrg.text
+                    else:
+                        itemOrg = 'Не указан'
+            
+                    print(f'{itemName.text} за {itemPrice.text} -> {itemLink}')
+                    additionArr(itemName.text, itemCity.text, itemPrice.text, itemDate.text[:10], itemOrg[:itemOrg.find('/')], itemLink)
+                else:
+                    soupAgroRussiaItem = getSoup(itemLink)
+                
+                    itemDate = soupAgroRussiaItem.find('time')
+                    itemCity = soupAgroRussiaItem.find('span', itemprop = 'addressLocality')
+                    itemOrg = soupAgroRussiaItem.find('div', class_ = 'ct_user_box_7_1')
+
+                    if itemOrg:
+                        itemOrg = itemOrg.text
+                    else:
+                        itemOrg = 'Не указан'
+            
+                    print(f'{itemName.text} за цена не указана -> {itemLink}')
+
+                    additionArr(itemName.text, itemCity.text, "Цена не указана", itemDate.text[:10], itemOrg[:itemOrg.find('/')], itemLink)
+
+            time.sleep(0.5)
+
+        print(f'[INFO]: Обработал {indexPage}/{len(pages) + 1}')
+
+        indexPage += 1
+
+
+# mainAgroRussia()
+
+if __name__ == "__main__":
+    start_time = time.time()
+    mainAgroServer()
+    time.sleep(1.5)
+    mainAgroRussia()
+    time.sleep(1.5)
+    excelEntry(arrName, arrCity, arrPrice, arrDate, arrOrg, arrLink)
+    print(time.time() - start_time)
+    
+
---
+++ grainParser2.py
@@ -0,0 +1,243 @@
+import requests
+import fake_useragent
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
+
+user = fake_useragent.UserAgent().random
+
+header = {
+    'user-agent': user
+    }
+
+#Cсылка на агро сервер поиск
+productName = {
+    "Сафлор": "saflor",
+    "Подсолнечник": "podsolnechnik",
+    "Лён": "len",
+    "Горчица": "gorchitsa"
+} 
+
+for key, value in productName.items():
+    print("Товар {0}: {1}".format(key, value))
+
+inputProductName = input("Введите товар на английском для парсинга сайта agroserver.ru ")
+
+linkAgroServerSearchSaflor = f'https://agroserver.ru/{inputProductName}/'
+#Cсылка на агро сервер
+linkAgroServer = 'https://agroserver.ru'
+
+
+#Ссылка на agro russia
+productID = {
+    "Сафлор": "790",
+    "Подсолнечник": "49",
+    "Лён": "53",
+    "Горчица": "54"
+}
+
+for key, value in productID.items():
+    print("ID Товара {0}: {1}".format(key, value))
+
+id_product = input("Введите id товара для парсинга сайта agro-russia.com ")
+linkAgroRussiaSearchSaflor = f'https://agro-russia.com/ru/trade/?adv_search=1&r_id={id_product}&types_id=2&page='
+
+linkAgroRussia = 'https://agro-russia.com/'
+
+arrName = []
+arrLink = []
+arrPrice = []
+arrCity = []
+arrDate = []
+arrOrg = []
+
+dictResult = {
+    'Название': '',
+    'Цена': '',
+    'Город': '', 
+    'Опубликовано': '',
+    'Организация': '',
+    'Ссылка': ''
+}
+
+#Функция получения кода страницы
+def getSoup(link):
+    response = requests.get(link, headers = header)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    return soup
+
+#Добавление в массивы
+def additionArr(name, city, price, date, org, link):
+    arrName.append(name)
+    arrLink.append(link)
+    arrCity.append(city)
+    arrPrice.append(price)
+    arrDate.append(date)
+    arrOrg.append(org)
+
+#Функция записи данных в excel
+def excelEntry(name, city, price, date, org, link):
+        dictResult['Название'] = name
+        dictResult['Город'] = city
+        dictResult['Цена'] = price
+        dictResult['Опубликовано'] = date
+        dictResult['Организация'] = org
+        dictResult['Ссылка'] = link
+
+        dataFrame = pd.DataFrame(dictResult)
+        dataFrame.to_excel("./Dad.xlsx", index=False) 
+
+        print('Успешно перезаписан excel-файл')
+
+#Информация с Агро Сервера
+def mainAgroServer():
+    print(f'[INFO]: Началась обработка с сайта {linkAgroServer}')
+
+    indexPage = 1
+    
+    soupAgroServer = getSoup(linkAgroServerSearchSaflor)
+
+    #Получение кол-ва страниц
+    try:
+        pages = soupAgroServer.find('ul', class_ = 'pg').find_all('li')
+    except:
+        pages = ['1']
+    
+    while indexPage <= len(pages):
+        # currentLink = 'https://agroserver.ru/len/Y2l0eT18cmVnaW9uPXxjb3VudHJ5PXxtZXRrYT18c29ydD0x/' + str(indexPage) + "/"
+        currentLink = linkAgroServerSearchSaflor
+
+        soupAgroServerPage = getSoup(currentLink)
+
+        #Получение всех товаров на странице
+        itemsAgroServer = soupAgroServerPage.find_all('div', class_ = 'line')
+        
+        for i in range(0, len(itemsAgroServer)):
+            
+            #Берём имя товара и ссылку на товар
+            try:
+                itemName = itemsAgroServer[i].find('div', class_ = 'th')
+                itemLink = linkAgroServer + itemName.find('a').get('href')
+                itemName = itemName.text
+            except:
+                itemName = "Без имени"    
+
+            #Берём цену товара
+            try:
+                itemPrice = itemsAgroServer[i].find('div', class_ = 'price').text
+            except:
+                itemPrice = 'цена не указана'
+            
+            #Когда опубликовали товар
+            try:
+                itemData = itemsAgroServer[i].find('div', class_ = 'date').text.strip()
+            except:
+                itemData = 'не указано, когда опубликовано'
+
+            #Организация, продающая товар
+            try:
+                itemOrg = itemsAgroServer[i].find('a', class_ = 'personal_org_menu').text.strip()
+            except:
+                itemOrg = 'компания не указана'
+
+            #Город товара
+            try:
+                itemGeo = itemsAgroServer[i].find('div', class_ = 'geo').text.strip()
+                #Если город начинается с г.
+                if itemGeo.find('г.') == 0:
+                    #Записываем без г.
+                    itemGeo = itemGeo[3:].strip()
+                #Если город начинается с доставка
+                elif itemGeo.find('доставка') == 0:
+                    itemGeo = itemGeo.strip()
+                else:
+                    itemGeo = itemGeo.strip()
+            except:
+                itemGeo = 'город не указан'
+            
+            print(f"{itemName} за {itemPrice} опубликован {itemData} у {itemOrg}-> {itemLink}")
+            additionArr(itemName, itemGeo, itemPrice, itemData, itemOrg, itemLink)
+
+        time.sleep(1)
+        print(f'[INFO]: Обработал {indexPage}/{len(pages)}')
+
+        indexPage += 1
+
+def mainAgroRussia():
+    print(f'[INFO]: Началась обработка с сайта {linkAgroRussia}')
+
+    indexPage = 1
+
+    soupAgroRussia = getSoup(linkAgroRussiaSearchSaflor + str(indexPage))
+
+    #Получение кол-ва страниц
+    try:
+        pages = soupAgroRussia.find('span', class_ = 'list').find_all('a', class_ = 'page')
+    except:
+        pages = []
+
+    while indexPage <= len(pages) + 1:
+        currentLink = linkAgroRussiaSearchSaflor + str(indexPage)
+
+        soupAgroRussiaPage = getSoup(currentLink)
+
+        #Получение всех товаров на странице
+        itemsAgroRussia = soupAgroRussiaPage.find_all('div', class_ = 'i_l_i_c_mode3')
+
+        for i in range(0, len(itemsAgroRussia)):
+            
+            #Берём имя товара
+            itemName = itemsAgroRussia[i].find('a', class_ = 'i_title')
+            
+            if itemName:
+                #Ссылка товара
+                itemLink = itemName.get('href')
+
+                #Берём цену на товар
+                try:
+                    itemPrice = itemsAgroRussia[i].find('span', class_ = 'i_price').text
+                except:
+                    itemPrice = 'цена не указана'
+
+                soupAgroRussiaItem = getSoup(itemLink)
+                
+                #Когда опубликовали товар
+                try:
+                    itemDate = soupAgroRussiaItem.find('time').text[:10]
+                except:
+                    itemDate = 'не указано, когда опубликовано'
+
+                #Город товара
+                try: 
+                    itemCity = soupAgroRussiaItem.find('span', itemprop = 'addressLocality').text
+                except:
+                    itemCity = 'город не указан'
+                
+                #Организация, продающая товар
+                try:
+                    itemOrg = soupAgroRussiaItem.find('div', class_ = 'ct_user_box_7_1').text[:itemOrg.find('/')]                 
+                except:
+                    itemOrg = 'компания не указана'
+
+                print(f'{itemName.text} за {itemPrice} -> {itemLink}')
+                additionArr(itemName.text, itemCity, itemPrice, itemDate, itemOrg, itemLink)
+
+        time.sleep(1)
+        print(f'[INFO]: Обработал {indexPage}/{len(pages) + 1}')
+
+        indexPage += 1
+
+def main():
+    start_time = time.time()
+    mainAgroServer()
+    time.sleep(1.5)
+    mainAgroRussia()
+    time.sleep(1.5)
+    excelEntry(arrName, arrCity, arrPrice, arrDate, arrOrg, arrLink)
+    print(time.time() - start_time)
+    print(input("[INFO]: Программа успешно завершена -> для выхода нажмите любую кнопку... "))
+
+if __name__ == "__main__":
+    main()
+
